059dbd82bcbcd91ed63c8ef985dd0455c59b63e6
MyMug <7french@gmail.com>
2017-03-19 07:07:27 +0100

    Create Contact.dutch.php

---
+++ Contact.dutch.php
@@ -0,0 +1,27 @@
+<?php
+
+$txt['contact_us'] = 'Contact';
+
+
+$txt['send_message'] = 'Plaats melding';
+$txt['contact_name'] = 'Je naam';
+$txt['contact_subject'] = 'Onderwerp van jouw melding';
+$txt['contact_body'] = 'Inhoud melding (GEEN hulp of ondersteunings vraagstukken),die kunnen worden geplaatst in een v/d categorieën als topic op het forum zelf';
+$txt['contact_email'] = 'Je E-mail adres';
+$txt['error_no_contact_name'] = 'Geen naam ingevuld';
+$txt['error_no_contact_email'] = 'Geen geldig E-mail ingevuld';
+$txt['error_no_contact_subject'] = 'Geen onderwerp ingevuld';
+$txt['error_no_contact_body'] = 'Geen inhoud melding ingevuld';
+
+$txt['contact_errors'] = 'Er zijn problemen met je melding';
+
+$txt['cannot_view_contact_page'] = 'je mag de contact pagina niet bekijken';
+
+$txt['contact_message'] = 'Een gebruiker heeft via het contactformulier een melding geplaatst';
+$txt['user_name'] = 'Gebruikersnaam: %1$s';
+$txt['user_email'] = 'Gebruikers E-mail adres: %1$s';
+$txt['user_subject'] = '(contact formulier) %1$s';
+$txt['user_body'] = 'Meldingen:' . "\n\n" . '%1$s';
+
+$txt['message_sent'] = 'Melding is verstuurd! je kunt <a href = "<URL>"> terug naar de index </a> als je dit wilt.';
+?>
