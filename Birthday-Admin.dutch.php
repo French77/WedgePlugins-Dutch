9548715f2b0f5d98cf7b0912f9b6ac343840a622
MyMug <7french@gmail.com>
2017-03-19 07:42:49 +0100

    Create Birthday-Admin.dutch.php

---
+++ Birthday-Admin.dutch.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Wedge: Birthdays
+ *
+ * Language strings for the administration panel.
+ *
+ * @package wedge-birthdays
+ * @copyright 2010-2011 Wedge Team, wedge.org, 2011 Simple Machines, simplemachines.org
+ * @license http://wedge.org/license/
+ */
+
+$txt['birthdays'] = 'Verjaardagen';
+$txt['birthday_info_center'] = 'Toon de aankomende verjaardagen in het info center?';
+$txt['birthday_info_center_days'] = 'Voor hoeveel dagen?';
+$txt['scheduled_task_birthdays'] = 'Verjaardagen voortgang';
+$txt['scheduled_task_desc_birthdays'] = 'Dagelijkse check is nodig, inclusief verzenden notificaties.';
+
+$txt['birthday_show_ages'] = 'Toon leeftijd';
+
+$txt['birthday_send_email'] = 'verstuur verjaardags bericht naar leden?';
+$txt['birthday_email'] = 'Verjaardags bericht om te gebruiken';
+$txt['birthday_body'] = 'Email inhoud';
+$txt['birthday_subject'] = 'Email onderwerp';
+
+$txt['help_birthday_email'] = 'Kies de te gebruiken index verjaardags e-mailbericht.Voorbeeld wordt getoond in the Email ontwerp en Email velden.<br><strong>Note:</strong> Verjaardagen moeten geactiveerd staan in geplande taken,welke zichtbaar is na installatie plugin.te controleren in <a href="<URL>?action=admin;area=scheduledtasks" target="_blank" class="new_win">Scheduled Tasks</a> pagina.';
+
+?>
