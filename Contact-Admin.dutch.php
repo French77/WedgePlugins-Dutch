52c74402ec56d878ba8bf3d525f5e4833a0fc35a
MyMug <7french@gmail.com>
2017-03-19 07:09:49 +0100

    Create Contact-Admin.dutch.php

---
+++ Contact-Admin.dutch.php
@@ -0,0 +1,13 @@
+<?php
+
+$txt['contact_alternate_email_addr'] = 'Alternatieve e-mailadressen om berichten naar toe te sturen.';
+$txt['contact_alt_email_sub'] = 'Indien opgegeven, een per regel.';
+
+$txt['contact_verification'] = 'Gebruikers moeten een CAPTCHA / verificatie invullen.';
+$txt['contact_verification_none'] = 'Nee';
+$txt['contact_verification_guests'] = 'Alleen Gasten';
+$txt['contact_verification_everyone'] = 'Iedereen';
+
+$txt['view_contact_page'] = 'Bekijk en gebruik "contact" pagina.';
+
+?>
