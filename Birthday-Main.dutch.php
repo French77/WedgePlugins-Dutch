3960099bde78490a8f1886f67322183b57eb426a
MyMug <7french@gmail.com>
2017-03-19 07:45:37 +0100

    Create Birthday-Main.dutch.php

---
+++ Birthday-Main.dutch.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Wedge: Birthdays
+ *
+ * Language strings for the main birthday's display.
+ *
+ * @package wedge-birthdays
+ * @copyright 2010-2011 Wedge Team, wedge.org, 2011 Simple Machines, simplemachines.org
+ * @license http://wedge.org/license/
+ */
+
+$txt['birthdays_upcoming'] = array(
+	1 => 'Jarig vandaag,van harte',
+	'n' => 'Jarig deze week',
+);
